2b49b9f7ab6e1067dec3e3fafcedb44d714d0622
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2021-07-02 08:20:30 +0530

    Added animation curves for all walking IK animations

--- Trans Walking Sim Unity/Assets/Scripts/Player/IK Scripts/IKObjectGrabber.cs
+++ Trans Walking Sim Unity/Assets/Scripts/Player/IK Scripts/IKObjectGrabber.cs
@@ -6,10 +6,15 @@ public class IKObjectGrabber : MonoBehaviour
     [HideInInspector] public bool objectInRange = false;
     [HideInInspector] public bool canLookAtObject = false;
 
-    [SerializeField] bool ikActive = false;
+    
+    [SerializeField] float reactionTime = 0.5f;
 
     private Transform lookObj = null;
     private Transform rightHandObj = null;
+
+    private bool playerShouldHoldObject = false;
+    private float state = 0f;
+    private float elapsedTime = 0f;
 
     private CanLookAtObject objectLookScript;
     private CanHoldObject objectHoldScript;
@@ -17,40 +22,66 @@ public class IKObjectGrabber : MonoBehaviour
 
     private void Start() {
         animator = GetComponent<Animator>();
+        state = 0;
         objectLookScript = FindObjectOfType<CanLookAtObject>();
         objectHoldScript = FindObjectOfType<CanHoldObject>();
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
-            ikActive = !ikActive;
+            playerShouldHoldObject = !playerShouldHoldObject;
         }
     }
 
-    private void OnAnimatorIK(int layerIndex) {
+    private void OnAnimatorIK() {
         if (animator) {
-            if (ikActive) {
-                // Attach the objects
-                AttachObjectTransforms();
+            // Attach the objects
+            AttachObjectTransforms();
 
-                // Set the look target position, if one has been assigned
-                if (lookObj != null && canLookAtObject) {
-                    animator.SetLookAtWeight(1f);
-                    animator.SetLookAtPosition(lookObj.position);
-                }
+            // Set the look target position, if one has been assigned
+            if (lookObj != null && canLookAtObject) {
+                animator.SetLookAtWeight(1f);
+                animator.SetLookAtPosition(lookObj.position);
+            }
 
-                // Set the right hand position and rotation, if one has been assigned
+            // Set the right hand position and rotation, if one has been assigned
+            if (playerShouldHoldObject) {
                 if (rightHandObj != null && objectInRange) {
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+                    if (state < 1.0f) {
+                        elapsedTime += Time.deltaTime;
+                        state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
+                    }
+                    else {
+                        state = 1.0f;
+                        elapsedTime = 0;
+                    }
+
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
+
                     animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
                     animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
                 }
             }
             else {
-                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0f);
-                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0f);
-                animator.SetLookAtWeight(0f);
+                if (state > 0f) {
+                    elapsedTime += Time.deltaTime;
+                    state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
+                    state = 1 - state;
+
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
+
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+                else {
+                    state = 0;
+                    elapsedTime = 0;
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0f);
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0f);
+                    animator.SetLookAtWeight(0f);
+                }
             }
         }
     }
