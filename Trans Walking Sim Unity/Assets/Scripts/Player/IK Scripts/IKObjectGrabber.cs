f7b9afebf3b804c12b5d1a49e19257712f949f82
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2021-07-03 17:37:07 +0530

    Improved hand IK
    Todo: Transition from IK to idle animation when holding items

--- Trans Walking Sim Unity/Assets/Scripts/Player/IK Scripts/IKObjectGrabber.cs
+++ Trans Walking Sim Unity/Assets/Scripts/Player/IK Scripts/IKObjectGrabber.cs
@@ -6,7 +6,6 @@ public class IKObjectGrabber : MonoBehaviour
     [HideInInspector] public bool objectInRange = false;
     [HideInInspector] public bool canLookAtObject = false;
 
-    
     [SerializeField] float reactionTime = 0.5f;
 
     private Transform lookObj = null;
@@ -38,52 +37,17 @@ public class IKObjectGrabber : MonoBehaviour
             // Attach the objects
             AttachObjectTransforms();
 
-            // Set the look target position, if one has been assigned
-            if (lookObj != null && canLookAtObject) {
-                animator.SetLookAtWeight(1f);
-                animator.SetLookAtPosition(lookObj.position);
-            }
+            // Set the look target position, if one has been assigned            
+            LookAtObject();
 
             // Set the right hand position and rotation, if one has been assigned
-            if (playerShouldHoldObject) {
-                if (rightHandObj != null && objectInRange) {
-                    if (state < 1.0f) {
-                        elapsedTime += Time.deltaTime;
-                        state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
-                    }
-                    else {
-                        state = 1.0f;
-                        elapsedTime = 0;
-                    }
-
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
-
-                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
-                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
-                }
-            }
-            else {
-                if (state > 0f) {
-                    elapsedTime += Time.deltaTime;
-                    state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
-                    state = 1 - state;
-
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
-
-                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
-                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
-                }
-                else {
-                    state = 0;
-                    elapsedTime = 0;
-                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0f);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0f);
-                    animator.SetLookAtWeight(0f);
-                }
-            }
+            HoldObject();
         }
+    }   
+
+    private GameObject GetMainObject() {
+        GameObject mainObject = rightHandObj.transform.parent.gameObject;
+        return mainObject;
     }
 
     private void AttachObjectTransforms() {
@@ -95,6 +59,76 @@ public class IKObjectGrabber : MonoBehaviour
         // Attach the grabbing object
         if (objectInRange) {
             rightHandObj = objectHoldScript.objectToHold;
+        }
+    }
+
+    private void LookAtObject() {
+        if (lookObj != null && canLookAtObject) {
+            if (state < 1.0f) {
+                elapsedTime += Time.deltaTime;
+                state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
+            }
+            else {
+                state = 1.0f;
+                elapsedTime = 0;
+            }
+
+            animator.SetLookAtWeight(state);
+            animator.SetLookAtPosition(lookObj.position);
+        }
+    }
+
+    private void HoldObject() {
+        if (playerShouldHoldObject) {
+            if (rightHandObj != null && objectInRange) {
+                if (state < 1.0f) {
+                    elapsedTime += Time.deltaTime;
+                    state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
+                }
+                else {
+                    state = 1.0f;
+                    elapsedTime = 0;
+                }
+
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
+
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+
+                GameObject mainObject = GetMainObject();
+
+                mainObject.transform.parent = this.transform;
+                mainObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        else {
+            if (state > 0f) {
+                elapsedTime += Time.deltaTime;
+                state = Mathf.Lerp(0, 1, elapsedTime / reactionTime);
+                state = 1 - state;
+
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, state);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, state);
+
+                if (rightHandObj != null) {
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+                    animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+
+                    GameObject mainObject = GetMainObject();
+
+                    mainObject.transform.parent = null;
+                    mainObject.GetComponent<Rigidbody>().isKinematic = false;
+                }
+            }
+            else {
+                state = 0;
+                elapsedTime = 0;
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0f);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0f);
+
+                animator.SetLookAtWeight(0f);
+            }
         }
     }
 }
