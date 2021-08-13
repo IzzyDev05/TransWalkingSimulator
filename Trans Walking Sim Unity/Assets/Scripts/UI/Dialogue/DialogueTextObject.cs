7720eecb234c39952d8bb8a4d2b94bdbc6caf484
IzzyDev <66153582+IzzyDev05@users.noreply.github.com>
2021-08-13 09:21:43 +0530

    Updated UI with some animations

--- Trans Walking Sim Unity/Assets/Scripts/UI/Dialogue/DialogueTextObject.cs
+++ Trans Walking Sim Unity/Assets/Scripts/UI/Dialogue/DialogueTextObject.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
+using Febucci.UI;
 
 public class DialogueTextObject : MonoBehaviour
 {
     [SerializeField] TMP_Text myText;
+    [SerializeField] TextAnimatorPlayer textAnimatorPlayer;
 
     public void Setup(string newDialogue) {
-        myText.text = newDialogue;
+        //myText.text = newDialogue;
+        textAnimatorPlayer.ShowText(newDialogue);
     }
 }
---
+++ Trans Walking Sim Unity/Assets/Scripts/UI/TypeWriterController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Febucci.UI;
+using System.Collections;
+
+public class TypeWriterController : MonoBehaviour
+{
+    [SerializeField] float disableTime = 0.015f;
+
+    private GameObject choicePanel;
+    private TextAnimatorPlayer textAnimatorPlayer;
+    private Animator[] animArray;
+
+    private void Start() {
+        choicePanel = GameObject.FindGameObjectWithTag("choicePanel");
+        textAnimatorPlayer = GetComponent<TextAnimatorPlayer>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            textAnimatorPlayer.SkipTypewriter();
+        }
+
+        if (choicePanel) {
+            animArray = choicePanel.GetComponentsInChildren<Animator>();
+            print(animArray.Length);
+        }
+        else {
+            Debug.LogWarning("Choice panel was not found!");
+            return;
+        }
+    }
+
+    public void DisableAnimator() {
+        StartCoroutine(WaitBeforeDisable());
+    }
+
+    public void EnableAnimtor() {
+        foreach (Animator anim in animArray) {
+            anim.enabled = true;
+        }
+    }
+
+    private IEnumerator WaitBeforeDisable() {
+        yield return new WaitForSeconds(disableTime);
+
+        Animator[] tempAnimArray = animArray;
+        foreach (Animator anim in tempAnimArray) {
+            anim.enabled = false;
+        }
+    }
+}
